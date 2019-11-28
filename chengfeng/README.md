a132e4007dfb915d79d68f4de769673c4a8aa15f
Chengfeng Lee <tolcf@qq.com>
2019-11-28 09:45:51 +0800

    delete chengfeng-dir

--- chengfeng/README.md
+++
@@ -1,2 +0,0 @@
-# chengfeng
-chengfeng
--- chengfeng/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	printf("_____________");
-	printf("*************");
-	printf("leechengfeng");
-	printf("hello world!");
-	return 0;
-}
